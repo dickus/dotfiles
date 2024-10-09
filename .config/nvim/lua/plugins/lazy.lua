540dbf5308091ccb7fa2c886b204c11b4e405fd3
dickus <dickie.dickus@proton.me>
2024-10-09 20:01:29 +0300

    cmdline for nvim and a little fix for lf

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -41,6 +41,7 @@ local plugins = {
         "L3MON4D3/LuaSnip",
         dependencies = {
             "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-cmdline",
             "hrsh7th/cmp-nvim-lsp",
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup({})
 require("mason-lspconfig").setup({
-    ensure_installed = { "bashls", "clangd", "lua_ls", "marksman", "rust_analyzer" },
+    ensure_installed = { "bashls", "clangd", "lua_ls", "marksman", "rust_analyzer", "pyright" },
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -19,6 +19,9 @@ lspconfig.marksman.setup({
     capabilities = capabilities,
 })
 lspconfig.rust_analyzer.setup({
+    capabilities = capabilities,
+})
+lspconfig.pyright.setup({
     capabilities = capabilities,
 })
 
