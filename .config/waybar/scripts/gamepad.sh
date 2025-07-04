4f3529f3d61dffad2ecc7a7a0f87b983f067a959
dickus <dickie.dickus@proton.me>
2025-07-04 17:41:54 +0300

    fix: updated tooltip

--- .config/waybar/scripts/gamepad.sh
+++ .config/waybar/scripts/gamepad.sh
@@ -10,15 +10,15 @@ if [[ ${DEVICE} ]]; then
     percent=$(upower -i "$DEVICE" | grep -Po 'percentage:\s*\K\d+')
 
     if [[ $percent -eq 100 ]]; then
-        printf '{"text": "", "class": "%s"}' "$HIGH"
+        printf '{"text": "", "class": "%s", "tooltip": "%s"}' "$HIGH" "$percent%"
     elif [[ $percent -gt 74 && $percent -ne 100 ]]; then
-        printf '{"text": "", "class": "%s"}' "$HIGH"
+        printf '{"text": "", "class": "%s", "tooltip": "%s"}' "$HIGH" "$percent%"
     elif [[ $percent -gt 49 && $percent -lt 75 ]]; then
-        printf '{"text": "", "class": "%s"}' "$MID"
+        printf '{"text": "", "class": "%s", "tooltip": "%s"}' "$MID" "$percent%"
     elif [[ $percent -gt 24 && $percent -lt 50 ]]; then
-        printf '{"text": "", "class": "%s"}' "$MID"
+        printf '{"text": "", "class": "%s", "tooltip": "%s"}' "$MID" "$percent%"
     else
-        printf '{"text": "", "class": "%s"}' "$LOW"
+        printf '{"text": "", "class": "%s", "tooltip": "%s"}' "$LOW" "$percent%"
     fi
 else
     printf '{"text": ""}'
