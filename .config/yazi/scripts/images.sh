8decde3295e9aa6560d99157017a79480841e01b
dickus <dickie.dickus@proton.me>
2025-07-14 09:02:26 +0300

    feat: sript for image manipulation

---
+++ .config/yazi/scripts/images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE="$1"
+MODE="$2"
+
+if file --mime-type -b "$FILE" | grep -q '^image/'; then
+    if [[ "$MODE" == "rotate" ]]; then
+        magick "$FILE" -rotate 90 "$FILE"
+    elif [[ "$MODE" == "rotate_back" ]]; then
+        magick "$FILE" -rotate -90 "$FILE"
+    elif [[ "$MODE" == "flip" ]]; then
+        magick "$FILE" -flip "$FILE"
+    elif [[ "$MODE" == "flop" ]]; then
+        magick "$FILE" -flop "$FILE"
+    fi
+fi
+
