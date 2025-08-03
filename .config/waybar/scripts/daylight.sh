ccc4a67efd98e83e13afc739c54bf511fa74f2d5
dickus <dickie.dickus@proton.me>
2025-08-03 13:29:32 +0300

    feat: LMB to change theme and light, RMB to change just light

--- .config/waybar/scripts/daylight.sh
+++ .config/waybar/scripts/daylight.sh
@@ -11,7 +11,19 @@ else
     printf '{"text": "%s", "class": "on"}' "$NIGHT"
 fi
 
-if ! [[ -z "$1" ]]; then
+if [[ "$1" == "toggle" ]]; then
+    if [[ "$TEMP" -eq 6000 ]]; then
+        hyprctl hyprsunset temperature 4500
+
+        $HOME/.config/scripts/theme_change/theme_schedule.sh dark
+    else
+        hyprctl hyprsunset temperature 6000; hyprctl hyprsunset identity
+
+        $HOME/.config/scripts/theme_change/theme_schedule.sh light
+    fi
+fi
+
+if [[ "$1" == "light" ]]; then
     if [[ "$TEMP" -eq 6000 ]]; then
         hyprctl hyprsunset temperature 4500
     else
