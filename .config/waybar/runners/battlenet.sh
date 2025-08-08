f5bdca9d5b94f651b094833bf7d18704bf7d1f8b
dickus <dickie.dickus@proton.me>
2025-08-08 14:56:45 +0300

    feat: run TSM, wowup and then battle.net

--- .config/waybar/runners/battlenet.sh
+++ .config/waybar/runners/battlenet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+env LUTRIS_SKIP_INIT=1 lutris lutris:rungameid/1 &
+
+wowup-cf
+
 env "$HOME/PortProton/data/scripts/start.sh" "$HOME/PortProton/data/prefixes/BATTLE_NET/drive_c/Program Files (x86)/Battle.net/Battle.net.exe"
 
