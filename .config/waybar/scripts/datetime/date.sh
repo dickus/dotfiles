ec7f025241ea4a9c70b190f979d3273759cf0d9c
dickus <dickie.dickus@proton.me>
2025-08-11 14:29:44 +0300

    fix: proper windows counting

--- .config/waybar/scripts/datetime/date.sh
+++ .config/waybar/scripts/datetime/date.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 
-windows=$(hyprctl activeworkspace -j | jq '.windows')
+windows=$(hyprctl activeworkspace -j | \
+    jq '.windows'
+)
 nwindows=$((windows))
 
-is_there_rofi=$(hyprctl clients | grep "rofi")
-is_there_todos=$(hyprctl clients | grep "todos")
-is_there_notes=$(hyprctl clients | grep "quicknote")
+workspace=$(hyprctl activeworkspace | \
+    grep "workspace" | \
+    head -n 1 | \
+    sed "s|.*(||; s|).*||"
+)
+nworkspace=$((workspace))
 
-if [[ "${is_there_rofi}" ]]; then
+is_there_rofi=$(hyprctl clients | \
+    grep "rofi" -A 5 | \
+    grep "workspace" | \
+    head -n 2 | \
+    tail -n 1 | \
+    sed "s|.*workspace: ||; s|(.*||"
+)
+nrofi=$((is_there_rofi))
+
+is_there_todos=$(hyprctl clients | \
+    grep "todos" -A 5 | \
+    grep "workspace" | \
+    head -n 2 | \
+    tail -n 1 | \
+    sed "s|.*workspace: ||; s|(.*||"
+)
+ntodos=$((is_there_todos))
+
+is_there_notes=$(hyprctl clients | \
+    grep "quicknote" | \
+    grep "workspace" | \
+    head -n 2 | \
+    tail -n 1 | \
+    sed "s|.*workspace: ||; s|(.*||"
+)
+nnotes=$((is_there_notes))
+
+if [[ "${nrofi}" -eq "${nworkspace}" ]]; then
     ((nwindows--))
 fi
 
-if [[ "${is_there_todos}" ]]; then
+if [[ "${nnotes}" -eq "${nworkspace}" ]]; then
     ((nwindows--))
 fi
 
-if [[ "${is_there_notes}" ]]; then
+if [[ "${ntodos}" -eq "${nworkspace}" ]]; then
     ((nwindows--))
 fi
 
