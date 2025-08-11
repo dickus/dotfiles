97b98bc57948516ec774149e137a56fa9ff0ddfc
dickus <dickie.dickus@proton.me>
2025-08-11 13:09:38 +0300

    feat: new zen mode

---
+++ .config/hypr/scripts/zen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+zen() {
+    pkill -f "waybar/config-left"
+    pkill -f "waybar/config-right"
+
+    hyprctl keyword general:gaps_out 8
+}
+
+nozen() {
+    hyprctl reload
+}
+
+if [[ $(ps aux | grep "waybar/config-left" | wc -l) != "1" ]]; then
+    zen
+else
+    nozen
+fi
+
