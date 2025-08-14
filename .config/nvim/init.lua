966b00a5e35a992679a5ee75aa894ceec979a198
dickus <dickie.dickus@proton.me>
2025-08-14 09:13:23 +0300

    feat: cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,10 +2,8 @@ require("settings")
 require("keys")
 
 require("plugins.lazy")
-require("plugins.undotree")
 require("plugins.lsp")
 require("plugins.lualine")
-require("plugins.highlight-colors")
 require("plugins.which-keys")
 require("plugins.cmps")
 require("plugins.obsidian")
@@ -13,14 +11,15 @@ require("plugins.footnote")
 require("plugins.snacks")
 require("plugins.luasnip")
 require("plugins.comfy-line-numbers")
+require("plugins.noice")
 
 --colorscheme
 require("themes.fjord")
 
 --basic settings
-vim.o.cmdheight = 2
-vim.wo.number = true --number of line
-vim.o.signcolumn = "yes"
-vim.o.foldmethod = "manual"
+vim.o.cmdheight      = 2
+vim.wo.number        = true --number of line
+vim.o.signcolumn     = "yes"
+vim.o.foldmethod     = "manual"
 vim.o.relativenumber = true
 
