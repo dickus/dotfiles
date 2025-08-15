82eaa79c8205da07fb11a8d95d969ab85641cce8
dickus <dickie.dickus@proton.me>
2025-08-15 17:56:56 +0300

    feat: don't suspend if there are windows on screen

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -2,8 +2,11 @@
 
 windows=$(hyprctl activeworkspace -j | jq '.windows')
 
-if [[ "${windows}" == "0" ]]; then
-    hyprlock
-fi
+action="${1}"
+
+case "${action}" in
+    "lock") [[ "${windows}" == "0" ]] && hyprlock ;;
+    "suspend") [[ "${windows}" == "0" ]] && systemctl suspend ;;
+esac
 
 
