583429deecee451552818faebb9829001fa15c0d
dickus <dickie.dickus@proton.me>
2025-10-07 11:26:01 +0300

    feat: a better approach to zen mode

--- .config/hypr/scripts/zen/zen.sh
+++ .config/hypr/scripts/zen/zen.sh
@@ -3,29 +3,36 @@
 MODE="${1}"
 script="${HOME}/.config/hypr/scripts/zen"
 mode_file="${script}/mode"
+theme_conf="${HOME}/.config/hypr/conf/theme.conf"
 
 zen() {
     echo "zen" > "${mode_file}"
 
-    killall waybar
+    sed -i "2s|.*||" "${theme_conf}"
 
-    hyprctl keyword general:gaps_out 100, 200
+    sed -i "s|gaps_out =.*|gaps_out = 150, 250|" "${theme_conf}"
+
+    hyprctl reload
 }
 
 nobars() {
     echo "nobars" > "${mode_file}"
 
-    killall waybar
+    sed -i "2s|.*||" "${theme_conf}"
 
-    hyprctl keyword general:gaps_out 8
+    sed -i "s|gaps_out =.*|gaps_out = 8|" "${theme_conf}"
+
+    hyprctl reload
 }
 
 bars() {
     echo "bars" > "${mode_file}"
 
-    WAYBAR_CONFIG=~/.config/waybar/config-left waybar -c ~/.config/waybar/config-left -s ~/.config/waybar/style.css &
+    sed -i "2s|.*|exec = WAYBAR_CONFIG=~/.config/waybar/config-left waybar -c ~/.config/waybar/config-left -s ~/.config/waybar/style.css &|" "${theme_conf}"
 
-    hyprctl keyword general:gaps_out 8, 8, 8, 2
+    sed -i "s|gaps_out =.*|gaps_out = 8, 8, 8, 2|" "${theme_conf}"
+
+    hyprctl reload
 }
 
 case "${MODE}" in
